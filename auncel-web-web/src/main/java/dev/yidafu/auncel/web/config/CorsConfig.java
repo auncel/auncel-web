711655aa9286cf75abee5d241cae3ef9f0deb374
yidafu <tiaozuwushi@qq.com>
2020-04-20 22:18:41 +0800

    feata: cors config

---
+++ auncel-web-web/src/main/java/dev/yidafu/auncel/web/config/CorsConfig.java
@@ -0,0 +1,18 @@
+package dev.yidafu.auncel.web.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @url https://blog.csdn.net/tiangongkaiwu152368/article/details/81099169
+ * @url https://segmentfault.com/a/1190000018904390
+ */
+@Configuration
+public class CorsConfig  implements WebMvcConfigurer {
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://www.auncel.top", "http://localhost:5000")
+                .allowedMethods("GET", "HEAD", "POST","PUT", "DELETE", "OPTIONS");
+    }
+}
