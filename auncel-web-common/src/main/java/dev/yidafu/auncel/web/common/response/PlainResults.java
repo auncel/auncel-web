c189d5a65b670db035f2d28a090622349bbb82d3
yidafu <tiaozuwushi@qq.com>
2020-04-22 23:31:25 +0800

    refactor: PlainResults add a method

--- auncel-web-common/src/main/java/dev/yidafu/auncel/web/common/response/PlainResults.java
+++ auncel-web-common/src/main/java/dev/yidafu/auncel/web/common/response/PlainResults.java
@@ -1,25 +1,25 @@
 package dev.yidafu.auncel.web.common.response;
 
-import dev.yidafu.auncel.web.common.response.PlainResult;
-
 public class PlainResults {
 
-  public static <T> PlainResult<T> success(T data, String msg) {
-    PlainResult result = new PlainResult(true, data, 1, msg);
-    return result;
-  }
+    public static <T> PlainResult<T> success(T data) {
+        PlainResult result = new PlainResult(true, data, 1, "请求成功");
+        return result;
+    }
 
-  public static PlainResult error(Integer code, String msg) {
-      PlainResult result = new PlainResult();
-      result.setCode(code);
-      result.setMsg(msg);
-      result.setSuccess(false);
-      return result;
-  }
+    public static <T> PlainResult<T> success(T data, String msg) {
+        PlainResult result = new PlainResult(true, data, 1, msg);
+        return result;
+    }
 
-  public static <T> PlainResult success(T data, Integer code, String msg) {
-      PlainResult<T> result = PlainResults.success(data, msg);
-      result.setCode(code);
-      return result;
-  }
+    public static PlainResult error(Integer code, String msg) {
+        PlainResult result = new PlainResult(false, null, code, msg);
+        return result;
+    }
+
+    public static <T> PlainResult success(T data, Integer code, String msg) {
+        PlainResult<T> result = PlainResults.success(data, msg);
+        result.setCode(code);
+        return result;
+    }
 }
