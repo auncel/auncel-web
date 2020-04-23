9c24c678afa0cd7ad1b8e09269df7203cfecbf38
yidafu <tiaozuwushi@qq.com>
2020-04-23 23:12:45 +0800

    func: update util

---
+++ auncel-web-common/src/main/java/dev/yidafu/auncel/web/common/utils/UpdateUtil.java
@@ -0,0 +1,28 @@
+package dev.yidafu.auncel.web.common.utils;
+
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.BeanWrapper;
+import org.springframework.beans.BeanWrapperImpl;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/**
+ * @url https://www.bbsmax.com/A/x9J2PlnNd6/
+ */
+public class UpdateUtil {
+    public static String[] getNullPropertyNames(Object source) {
+        final BeanWrapper src = new BeanWrapperImpl(source);
+        java.beans.PropertyDescriptor[] pds = src.getPropertyDescriptors();
+
+        Set<String> emptyNames = new HashSet<>();
+        for (java.beans.PropertyDescriptor pd : pds) {
+            Object srcValue = src.getPropertyValue(pd.getName());
+            if (srcValue == null) {
+                emptyNames.add(pd.getName());
+            }
+        }
+        String[] result = new String[emptyNames.size()];
+        return emptyNames.toArray(result);
+    }
+}
