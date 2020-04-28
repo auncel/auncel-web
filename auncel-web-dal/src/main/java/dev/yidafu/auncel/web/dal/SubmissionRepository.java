0de8ab2eba8b42872813cfeac0a4d5668b796bee
yidafu <tiaozuwushi@qq.com>
2020-04-28 23:03:21 +0800

    func: get submission by userId and problemId

--- auncel-web-dal/src/main/java/dev/yidafu/auncel/web/dal/SubmissionRepository.java
+++ auncel-web-dal/src/main/java/dev/yidafu/auncel/web/dal/SubmissionRepository.java
@@ -8,11 +8,15 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 public interface SubmissionRepository extends JpaRepository<Submission, Long> {
 
     // https://blog.csdn.net/liuchuanhong1/article/details/69950886
-    @Query(value = "SELECT id, created_at, updated_at, a_css, a_html, logs, problem_id, score, screenshot, status, submiter_id FROM submission WHERE problem_id=:problem_id AND submiter_id=:submiter_id LIMIT 1", nativeQuery = true)
+    @Query(value = "SELECT id, created_at, updated_at, style, html, logs, problem_id, score, screenshot, status, submiter_id FROM submission WHERE problem_id=:problem_id AND submiter_id=:submiter_id LIMIT 1", nativeQuery = true)
     public Submission selectByProblemIdAndUserId(@Param("problem_id") Long ProblmeId, @Param("submiter_id") Long userId);
 
     public Submission getFirstByProblem(Problem problem);
+
+    public List<Submission> findAllBySubmiterAndProblem(User submiter, Problem problem);
 }
