3923583785a6df5f163708885fe188e26dca7e79
yidafu <tiaozuwushi@qq.com>
2020-05-06 23:20:24 +0800

    refactor: new query method

--- auncel-web-dal/src/main/java/dev/yidafu/auncel/web/dal/ContestRepository.java
+++ auncel-web-dal/src/main/java/dev/yidafu/auncel/web/dal/ContestRepository.java
@@ -1,6 +1,7 @@
 package dev.yidafu.auncel.web.dal;
 
 import dev.yidafu.auncel.web.domain.Contest;
+import dev.yidafu.auncel.web.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
@@ -8,4 +9,6 @@ import java.util.List;
 public interface ContestRepository extends JpaRepository<Contest, Long> {
     @Override
     public List<Contest> findAll();
+
+    public List<Contest> findAllByMaker(User maker);
 }
--- auncel-web-dal/src/main/java/dev/yidafu/auncel/web/dal/ProblemRepository.java
+++ auncel-web-dal/src/main/java/dev/yidafu/auncel/web/dal/ProblemRepository.java
@@ -1,6 +1,8 @@
 package dev.yidafu.auncel.web.dal;
 
 import dev.yidafu.auncel.web.domain.Problem;
+import dev.yidafu.auncel.web.domain.ProblemAccessType;
+import dev.yidafu.auncel.web.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
@@ -9,4 +11,6 @@ import java.util.List;
 public interface ProblemRepository  extends JpaRepository<Problem, Long> {
     @Override
     List<Problem> findAll();
+
+    List<Problem> findAllByMakerOrAccess(User maker, ProblemAccessType access);
 }
